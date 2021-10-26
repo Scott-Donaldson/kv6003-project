20ab9ea55658d8f87af8def4ceead61513db68d7
Scrub <scott-donaldson@outlook.com>
2021-10-27 00:07:49 +0100

    Added Comments and more robust message parsing for commands

--- index.js
+++ index.js
@@ -8,26 +8,34 @@ let classifier = new Classifier(Classifier.defaultThreashold())
 import 'dotenv/config'
 
 const PREFIX = config.prefix
-//discord.com/oauth2/authorize?client_id=897117855806537739&permissions=8&scope=bot
 
 client.login(process.env.TOKEN)
 
+/**
+ * Client Ready Event Listener
+ */
 client.on('ready', ()=>{
     console.log(`[ BOT ] ${client.user.username} is online!`)
     client.user.setPresence({activities: [{name: "your messages", type: "WATCHING"}]})
 })
 
+/**
+ * Client messageCreate Event Listener
+ * Triggers on every message sent.
+ * Depending on if the message contains the prefix the bot will either handle the command or run the message through its classifier
+ */
 client.on('messageCreate', async message => {
     let allowedChannels = config.allowedChannels
     if(!allowedChannels.includes(message.channel.id)) return;
 
     if(message.author.bot) return;
     if(message.content.startsWith(PREFIX)){
-        let args = message.content.split(" ")
+        let args = message.content.slice(PREFIX.length).trim().split(/ +/)
+        let cmd = args[0]
         //COMMAND HANDLING
     }else{
         //General Message Moderation
-        let res = await classifier.classifyMessage(message.content, 0.65)
+        let res = await classifier.classifyMessage(message.content)
         console.log(res)
     }
 })
