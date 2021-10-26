148ea1993a5056bd6a976122a9bd24fe9c2ee052
Scrub <scott-donaldson@outlook.com>
2021-10-26 16:30:10 +0100

    MVP created
    Still no command handling
    Turned classifier into Class
    Basic console output that gives T/F values for toxicity
    Created Config File
    Got TensorFlow install to work, dont know if it will on other machines yet.

--- Utils/classifier.js
+++ Utils/classifier.js
@@ -1,16 +1,33 @@
-import * as tf from '@tensorflow/tfjs'
+import '@tensorflow/tfjs-node'
 import * as toxicmodel from '@tensorflow-models/toxicity'
 
-tf.setBackend('cpu')
+class Classifier {
+    constructor(threashold){
+        if(isNaN(threashold)) throw new Error(`Threshold parameted must be a number! Got ${threashold} (${typeof(threashold)})`)
+        this._threashold = threashold
+    }
+    get threashold(){
+        return this._threashold
+    }
+    set threashold(newThreashold){
+        this._threashold = newThreashold
+    }
 
-const classifyMessage = (message, threshold) => {
-    message[0] = message;
-    toxicmodel.load(threshold).then(model => {
-        model.classify(message).then(output => {
-            return output;
+    classifyMessage = async message => {
+        let model = await toxicmodel.load(this.threashold)
+        let res = await model.classify([message])
+        return this.parseClassifiedMessage(res)
+    }
+    parseClassifiedMessage = results => {
+        let resultObject = {}
+        results.forEach(element => {
+            resultObject[element.label] = element.results[0].match || false
         })
-    })
+        return resultObject
+    }
+    static defaultThreashold(){
+        return 0.65
+    }
 }
 
-
-export {classifyMessage}
+export {Classifier}
---
+++ Utils/config.js
@@ -0,0 +1,4 @@
+export default{
+    "prefix": "!",
+    "allowedChannels": ["902198800624521257","902198909676441680"]
+}
