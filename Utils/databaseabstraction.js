2a58bf336f9aafa04b6bfbce0f91e8535140f603
Scrub <scott-donaldson@outlook.com>
2021-12-16 16:51:37 +0000

    DBA tool functions added for getting and incrementing counts

--- Utils/databaseabstraction.js
+++ Utils/databaseabstraction.js
@@ -14,4 +14,20 @@ export default class DatabaseAbstraction {
     const sql = `SELECT value FROM '${config.DATABASE_CONFIG.TABLES.TABLE_CONFIG.name}' WHERE name = ?`
     return parseFloat(this.connection.prepare(sql).get('threashold').value)
   }
+
+  getStatCount (stat) {
+    const sql = `SELECT value FROM '${config.DATABASE_CONFIG.TABLES.TABLE_STATS.name}' WHERE statistic = ?`
+    return parseInt(this.connection.prepare(sql).get(stat).value)
+  }
+
+  getRowCount (table) {
+    const sql = 'SELECT COUNT(*) from ?'
+    return parseInt(this.connection.prepare(sql).get(table))
+  }
+
+  incrementCount (stat) {
+    const current = this.getStatCount(stat)
+    const sql = `UPDATE ${config.DATABASE_CONFIG.TABLES.TABLE_STATS.name} SET value = ${current + 1} WHERE statistic = ?`
+    this.connection.prepare(sql).run(stat)
+  }
 }
